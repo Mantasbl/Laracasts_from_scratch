55919eda60c6d5f79587150a92ed9c57f5b0215e
MantasBl <mantas.baltakis@gmail.com>
2019-03-11 09:33:36 +0000

    Episode 17 Form Actions

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,13 @@
     <title>Hello Bulma!</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <style type="text/css">
+
+      .is-complete {
+        text-decoration: line-through;
+      }
+
+    </style>
   </head>
   <body>
   <section class="section">
